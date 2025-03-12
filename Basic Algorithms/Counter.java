d304e554e58adcf23c988f3280dc345a9017d952
Nandish BS <nandishbs11@gmail.com>
2025-03-12 22:31:54 +0530

    initialized

---
+++ Basic Algorithms/Counter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Counter {
+    static int countDigits(int n){
+            int count = 0;
+            while(n>0){
+                n = n / 10;
+                count++;
+            }
+            return count;
+        }
+    
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number : ");
+            int n = sc.nextInt();
+            System.out.println(countDigits(n));
+            sc.close();
+    }
+}
